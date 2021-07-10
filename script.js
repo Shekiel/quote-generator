ebf346e743a8320ce488833602355a026b3c3332
shekiel <shekielmorris1@gmail.com>
2021-07-10 22:53:42 +0100

    fetch and display data from API

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
 let apiQuotes = [];
 
 // Show New Quote
 function newQuote() {
   // Pick randon quote from apiQuotes array
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-  console.log(quote);
+  authorText.textContent = quote.author;
+  quoteText.textContent = quote.text;
 }
 
 // Get Quote From API
